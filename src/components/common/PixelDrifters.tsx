f4bde13651bcb3c8428586e9cc030d178b241d87
azy <90897554+azrayaal@users.noreply.github.com>
2026-07-14 12:57:14 +0700

    Update PixelDrifters.tsx

--- src/components/common/PixelDrifters.tsx
+++ src/components/common/PixelDrifters.tsx
@@ -70,7 +70,7 @@ export function PixelDrifters({ className }: PixelDriftersProps) {
                 }
           }
         >
-          <Sprite Size{drifters.size}/>
+          <Sprite size{drifters.size}/>
         </motion.div>
       ))}
     </div>
